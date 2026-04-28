13ec077bd8bad0c2ff1814d84ac6e09681fcb7b3
irineudsf <irineu.dsf98@gmail.com>
2026-04-28 05:36:00 -0400

    Configura número WhatsApp real

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,5 @@
 // Substitua pelo seu número do WhatsApp com DDI+DDD (ex: 5511999999999)
-export const WHATSAPP_NUMBER = 'SEUNUMERO'
+export const WHATSAPP_NUMBER = '16892426746'
 
 export const WHATSAPP_URL = `https://api.whatsapp.com/send?phone=${WHATSAPP_NUMBER}&text=Ol%C3%A1!%20Quero%20saber%20mais%20sobre%20o%20site%20da%20Zavo.`
 
